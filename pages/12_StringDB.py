4d90fa15d83dce781a1aeea0463a6c9351cad60b
Arnaud Ogier <cyrenaique@users.noreply.github.com>
2023-10-02 11:27:37 +0200

    commit strange

--- pages/12_StringDB.py
+++ pages/12_StringDB.py
@@ -232,9 +232,10 @@ if not df_inter.empty:
     # df_umap_cluster['chromosome']=df_umap_cluster['chromosome'].apply(int_to_str)
     if disp:
         st.write(df_umap_cluster)
-    # list_enr=[]
-    # for grpName, rows in df_clust.groupby('cluster'):
-    #     list_enr.append(get_stringDB_enr())
+    list_enr=[]
+    for grpName, rows in df_clust.groupby('cluster'):
+        # df_temp = get_stringDB_enr(rows['']))
+        st.write(rows)
     ################################### ENRICHMENT ##############################################
     list_cat=get_list_category(df_umap_cluster,'symbol')
     categ = st.selectbox("Select Category", list_cat)
