dd2bbc35fccf5c9be2300b47cd504d05a085e2f2
zahra <zahra.hanifehlou@ksilink.com>
2023-10-10 18:17:49 +0200

    gene symbol2

--- pages/3_Find_Similar_Profiles.py
+++ pages/3_Find_Similar_Profiles.py
@@ -48,7 +48,7 @@ else:
         choix = st.selectbox("Select the Profile", cpdnames)
     df_sel = cpd_pro[(cpd_pro["metasource"] == choix_source)
                      & (cpd_pro["metacpdname"] == choix)].head(1)
-    title=f' similar compounds to {choix} in  {choix_source}'
+    title = f' similar compounds to {choix} in  {choix_source}'
     with mainCols[1]:
         st.write("Selected Profile", df_sel)
 
@@ -65,10 +65,10 @@ else:
     sql_umqpemd = f"select * from umapemd where metasource='{choix_source}'"
     df_src_emd = sql_df(sql_umqpemd, profile_conn)
 
-
     # cpds---------------------------------------------------------------------------------------------------------------------
-    
-    cosine_sim_tab,knn_sim_tab = st.tabs([ "Similar CPDs with cosine", "Similar CPDs with KNN in UMAP  "])
+
+    cosine_sim_tab, knn_sim_tab = st.tabs(
+        ["Similar CPDs with cosine", "Similar CPDs with KNN in UMAP  "])
     with cosine_sim_tab:  # Similar CPDs with cosine
         thr_cols = st.columns(2)
         with thr_cols[0]:
@@ -94,42 +94,61 @@ else:
         df_results_cpd = pd.DataFrame()
         df_keep_prof_cpd = pd.DataFrame()
         if len(b_list_cpd) > 0:
-            sql_cpds = f"select cpd.pubchemid,cpd.keggid, cpd.cpdname, cpd.smile,cpdgene.geneid,cpdbatchs.batchid,keggcpd.efficacy from cpd \
+            sql_cpds = f"select cpd.pubchemid,cpd.keggid, cpd.cpdname, gene.symbol, cpd.smile,cpdgene.geneid,cpdbatchs.batchid,keggcpd.efficacy from cpd \
             inner join cpdbatchs on cpd.pubchemid=cpdbatchs.pubchemid \
             left join cpdgene on cpdbatchs.pubchemid=cpdgene.pubchemid \
+            left join gene on gene.geneid=cpdgene.geneid \
             left join keggcpd on cpd.keggid=keggcpd.keggid \
-            where cpdbatchs.batchid in ({','.join(b_list_cpd)}) group by cpd.pubchemid,cpd.keggid, cpd.cpdname, cpd.smile,cpdgene.geneid,cpdbatchs.batchid,keggcpd.efficacy"
+            where cpdbatchs.batchid in ({','.join(b_list_cpd)}) group by cpd.pubchemid,cpd.keggid, cpd.cpdname, gene.symbol,cpd.smile,cpdgene.geneid,cpdbatchs.batchid,keggcpd.efficacy"
             df_results_cpd = sql_df(sql_cpds, conn)
             df_results_cpd.drop_duplicates(subset=["pubchemid"], inplace=True)
+            df_keep_prof_cpd = df_source[df_source["metabatchid"].isin(
+                df_keep_cpd["metabatchid"].values)]
+            df_keep_prof_cpd.reset_index(inplace=True, drop=True)
+
             if len(df_results_cpd) > 0:
-                st.session_state["df_cpds"] = pd.concat(
-                    [df_results_cpd, df_cpds])
-                df_keep_prof_cpd = df_source[df_source["metabatchid"].isin(
-                    df_keep_cpd["metabatchid"].values)]
-                df_keep_prof_cpd.reset_index(inplace=True, drop=True)
                 df_keep_prof_cpd = df_keep_prof_cpd.merge(df_results_cpd.add_prefix(
                     'meta'), left_on='metabatchid', right_on='metabatchid').reset_index(drop=True)
                 df_keep_prof_cpd.loc[df_keep_prof_cpd.metacpdname ==
-                                    "No result", 'metacpdname'] = None
+                                     "No result", 'metacpdname'] = None
                 df_keep_prof_cpd['metacpdname'] = df_keep_prof_cpd['metacpdname'].str[:30]
                 df_keep_prof_cpd['metacpdname'] = df_keep_prof_cpd['metacpdname'].fillna(
                     df_keep_prof_cpd['metabatchid'])
 
         fig_clusmap_cpd = px.histogram(df_hist_cpd, x="sim")
         fig_clusmap_cpd.add_vline(x=thres_cpd)
-    
+
         st.plotly_chart(fig_clusmap_cpd, theme="streamlit",
                         use_container_width=True)
+        st.session_state["df_profiles"] = df_keep_prof_cpd
+        if choix_source == "CRISPER":
+            sql_crisper = f"SELECT gene.symbol, gene.geneid,crisperbatchs.batchid  FROM crisperbatchs  inner join gene \
+            on gene.geneid=crisperbatchs.geneid  where crisperbatchs.batchid in ({','.join(b_list_cpd)})  group by gene.symbol, gene.geneid,crisperbatchs.batchid "
+            df_keep_cpd = sql_df(
+                sql_crisper, conn).drop_duplicates(subset=["batchid"])
+        
+            df_prof_crisper = df_prof_crisper.merge(df_keep_cpd.add_prefix(
+                'meta'), left_on='metabatchid', right_on='metabatchid').reset_index(drop=True)
+      
+            meta_cols = [
+                col for col in df_keep_prof_cpd.columns if col.startswith("meta")]
+            df_keep_cpd[meta_cols] = df_keep_prof_cpd[meta_cols]
     
-        if len(df_results_cpd) > 0:
+            df_keep_cpd["metacpdname"] = df_keep_cpd["batchid"]
+            df_keep_prof_cpd["metacpdname"] = df_keep_prof_cpd["metabatchid"]
+            df_keep_cpd["efficacy"] = None
+            st.session_state["df_crisper"] = df_keep_prof_cpd
+
+        if len(df_keep_cpd) > 0:
             st.write("\n")
-            df_keep_cpd = df_keep_cpd.merge(
-                df_results_cpd, left_on='metabatchid', right_on='batchid').reset_index(drop=True)
-            df_keep_cpd = df_keep_cpd.drop(["metabatchid"], axis=1)
-            df_keep_cpd["source"]=choix_source
+            if choix_source != "CRISPER":
+                df_keep_cpd = df_keep_cpd.merge(
+                    df_results_cpd, left_on='metabatchid', right_on='batchid').reset_index(drop=True)
+                df_keep_cpd = df_keep_cpd.drop(["metabatchid"], axis=1)
+                df_keep_cpd["source"] = choix_source
 
             fig_cols1 = st.columns(2)
-            name=choix_source+choix
+            name = choix_source+choix
             with fig_cols1[0]:
                 st.write(f" {title} :  metadata")
                 st.write(df_keep_cpd)
@@ -140,32 +159,32 @@ else:
                 st.write(df_keep_prof_cpd.head(10))
                 st.download_button(
                     label="Save Profile", data=convert_df(df_keep_prof_cpd), file_name=f"{name}_cos_prof.csv", mime='csv',)
-            
+
             st.write("\n")
             fig_cols2 = st.columns(2)
             with fig_cols2[0]:
-                fig = px.pie(df_keep_cpd,  names='geneid',
-                                title=f'   {title} : geneid',
-                                )
+                fig = px.pie(df_keep_cpd,  names='symbol',
+                             title=f'   {title} : symbol',
+                             )
                 fig.update_traces(textposition='inside',
-                                    textinfo='percent+label')
+                                  textinfo='percent+label')
                 st.plotly_chart(fig, theme="streamlit",
                                 use_container_width=True)
             with fig_cols2[1]:
                 fig = px.pie(df_keep_cpd,  names='efficacy',
-                                title=f'{title} : efficacy',
-                                )
+                             title=f'{title} : efficacy',
+                             )
                 fig.update_traces(textposition='inside',
-                                    textinfo='percent+label')
+                                  textinfo='percent+label')
                 st.plotly_chart(fig, theme="streamlit",
                                 use_container_width=True)
-            
-            st.write("\n")# ----------plot sim cpds in UMAP
+
+            st.write("\n")  # ----------plot sim cpds in UMAP
             df_src_emd["color"] = "others"
             df_src_emd.loc[df_src_emd["metabatchid"].isin(
                 batch_list_cpd), "color"] = "similar compounds"
             df_src_emd.loc[df_src_emd["metacpdname"] ==
-                            choix, "color"] = "selected compounds"
+                           choix, "color"] = "selected compounds"
             fig = px.scatter(
                 df_src_emd,
                 x="umap1",
@@ -178,23 +197,22 @@ else:
             )
             st.plotly_chart(fig, theme="streamlit",
                             use_container_width=True)
-            
-            st.write("\n")# ----------plot PROFILE
-            tmp=df_keep_prof_cpd.head(15)
+
+            st.write("\n")  # ----------plot PROFILE
+            tmp = df_keep_prof_cpd.head(15)
             cpd_names = tmp.metacpdname.values
             df_plt = tmp.set_index("metacpdname")
             filter_col = [
                 col for col in df_plt.columns if not col.startswith("meta")]
             df_plt = df_plt[filter_col].T
             fig_clusmap = px.line(
-                df_plt, x=filter_col, y=cpd_names, width=1400, height=1000,title=f"{title} : 15 similar compounds")
+                df_plt, x=filter_col, y=cpd_names, width=1400, height=1000, title=f"{title} : 15 similar compounds")
             st.plotly_chart(fig_clusmap, theme="streamlit",
                             use_container_width=True)
-            
 
-            st.write("\n")# ----------plot PROFILE heatmap
+            st.write("\n")  # ----------plot PROFILE heatmap
             tmp = df_keep_prof_cpd.copy()
-            if len(tmp)>1:
+            if len(tmp) > 1:
                 plt_src, col_colors = get_col_colors(tmp)
                 fig_clusmap, ax1 = plt.subplots()
                 fig_clusmap = sns.clustermap(
@@ -207,7 +225,7 @@ else:
                     col_cluster=False,
                     cmap="vlag",
                     center=0,
-                
+
                     figsize=(16, len(plt_src)/2),
                 )
 
@@ -217,40 +235,41 @@ else:
         from sklearn.neighbors import NearestNeighbors
         nb_cluster = st.slider(
             'Number of neighbors', min_value=2, max_value=30, value=10, step=1)
-        
+
         X = df_src_emd[["umap1", "umap2"]].to_numpy()
         neigh = NearestNeighbors(n_neighbors=nb_cluster, n_jobs=-1)
         neigh.fit(X)
-        if choix in df_src_emd["metacpdname"].values: 
-       
+        if choix in df_src_emd["metacpdname"].values:
+
             points = df_src_emd[df_src_emd["metacpdname"]
                                 == choix][["umap1", "umap2"]]
-        
+
             distances, indices = neigh.kneighbors(points)
             nearest_neighbor_name = df_src_emd.loc[indices[0,
-                                                        1:], 'metacpdname']
-            nearest_neighbor_name=nearest_neighbor_name.to_list()
+                                                           1:], 'metacpdname']
+            nearest_neighbor_name = nearest_neighbor_name.to_list()
             nearest_neighbor_name.append(choix)
-        
+
             knn_sim_df = df_src_emd[df_src_emd["metacpdname"].isin(
                 nearest_neighbor_name)]
-            
+
             # knn_sim_df.drop(columns=["metasource"],inplace=True)
             df_keep_prof_cpd_knn = df_source[df_source["metabatchid"].isin(
-                        knn_sim_df["metabatchid"].values)]
+                knn_sim_df["metabatchid"].values)]
             df_keep_prof_cpd_knn.reset_index(inplace=True, drop=True)
-        
-            df_keep_prof_cpd_knn = df_keep_prof_cpd_knn.merge(knn_sim_df[["metabatchid","metaefficacy","metageneid","metagenesymbol","metasmile","metakeggid","metacpdname"]], left_on='metabatchid', right_on='metabatchid').reset_index(drop=True)
+
+            df_keep_prof_cpd_knn = df_keep_prof_cpd_knn.merge(knn_sim_df[["metabatchid", "metaefficacy", "metageneid", "metagenesymbol",
+                                                              "metasmile", "metakeggid", "metacpdname"]], left_on='metabatchid', right_on='metabatchid').reset_index(drop=True)
             df_keep_prof_cpd_knn.loc[df_keep_prof_cpd_knn.metacpdname ==
-                                "No result", 'metacpdname'] = None
+                                     "No result", 'metacpdname'] = None
             df_keep_prof_cpd_knn['metacpdname'] = df_keep_prof_cpd_knn['metacpdname'].str[:30]
             df_keep_prof_cpd_knn['metacpdname'] = df_keep_prof_cpd_knn['metacpdname'].fillna(
-                        df_keep_prof_cpd_knn['metabatchid'])
-            
+                df_keep_prof_cpd_knn['metabatchid'])
+
             if len(knn_sim_df) > 0:
                 st.write("\n")
                 fig_cols3 = st.columns(2)
-                name=choix_source+choix
+                name = choix_source+choix
                 with fig_cols3[0]:
                     st.write(f" {title} MetaData NN")
                     st.write(knn_sim_df)
@@ -261,32 +280,32 @@ else:
                     st.write(df_keep_prof_cpd_knn.head(10))
                     st.download_button(
                         label="Save Profile", data=convert_df(df_keep_prof_cpd_knn), file_name=f"{name}_knn_prof.csv", mime='csv',)
-                
+
                 st.write("\n")
                 fig_cols4 = st.columns(2)
                 with fig_cols4[0]:
                     fig = px.pie(knn_sim_df,  names='metagenesymbol',
-                                    title=' {title}  : gene symbol',
-                                    )
+                                 title=' {title}  : gene symbol',
+                                 )
                     fig.update_traces(textposition='inside',
-                                        textinfo='percent+label')
+                                      textinfo='percent+label')
                     st.plotly_chart(fig, theme="streamlit",
                                     use_container_width=True)
                 with fig_cols4[1]:
                     fig = px.pie(knn_sim_df,  names='metaefficacy',
-                                    title=' {title} : efficacy',
-                                    )
+                                 title=' {title} : efficacy',
+                                 )
                     fig.update_traces(textposition='inside',
-                                        textinfo='percent+label')
+                                      textinfo='percent+label')
                     st.plotly_chart(fig, theme="streamlit",
                                     use_container_width=True)
 
-                #-------------------------------------------
+                # -------------------------------------------
                 df_src_emd["color"] = "others"
                 df_src_emd.loc[df_src_emd["metacpdname"].isin(
                     nearest_neighbor_name), "color"] = "similar compounds"
                 df_src_emd.loc[df_src_emd["metacpdname"] ==
-                            choix, "color"] = "selected compounds"
+                               choix, "color"] = "selected compounds"
                 figUMAP_knn = px.scatter(
                     df_src_emd,
                     x="umap1",
@@ -304,41 +323,41 @@ else:
 
                 st.plotly_chart(figUMAP_knn, theme="streamlit",
                                 use_container_width=True)
-                
+
                 #
-                st.write("\n")# ----------plot PROFILE
-                tmp=df_keep_prof_cpd_knn.head(15)
+                st.write("\n")  # ----------plot PROFILE
+                tmp = df_keep_prof_cpd_knn.head(15)
                 cpd_names = tmp.metacpdname.values
                 df_plt = tmp.set_index("metacpdname")
                 filter_col = [
                     col for col in df_plt.columns if not col.startswith("meta")]
                 df_plt = df_plt[filter_col].T
                 fig_clusmap = px.line(
-                    df_plt, x=filter_col, y=cpd_names, width=1400, height=1000 , title=f"{title} : Profile")
+                    df_plt, x=filter_col, y=cpd_names, width=1400, height=1000, title=f"{title} : Profile")
                 st.plotly_chart(fig_clusmap, theme="streamlit",
                                 use_container_width=True)
-                
 
-                st.write("\n")# ----------plot PROFILE heatmap
+                st.write("\n")  # ----------plot PROFILE heatmap
                 tmp = df_keep_prof_cpd_knn.copy()
-                plt_src, col_colors = get_col_colors(tmp)
-                fig_clusmap, ax1 = plt.subplots()
-                fig_clusmap = sns.clustermap(
-                    plt_src,
-                    metric="cosine",
-                    col_colors=col_colors,
-                    # method="ward",
-                    xticklabels=False,
-                    yticklabels=True,
-                    col_cluster=False,
-                    cmap="vlag",
-                    center=0,
-                    vmin=-5,
-                    vmax=5,
-                    figsize=(16, len(plt_src)/2),
-                )
+                if len(tmp) > 1:
+                    plt_src, col_colors = get_col_colors(tmp)
+                    fig_clusmap, ax1 = plt.subplots()
+                    fig_clusmap = sns.clustermap(
+                        plt_src,
+                        metric="cosine",
+                        col_colors=col_colors,
+                        # method="ward",
+                        xticklabels=False,
+                        yticklabels=True,
+                        col_cluster=False,
+                        cmap="vlag",
+                        center=0,
+                        vmin=-5,
+                        vmax=5,
+                        figsize=(16, len(plt_src)/2),
+                    )
 
-                st.pyplot(fig_clusmap)
+                    st.pyplot(fig_clusmap)
 
-        
+
 
