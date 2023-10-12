2a62bbc00514bc5eec148b99baf8eed2128c2c00
cyrenaique <arno.ogier@gmail.com>
2023-10-12 09:59:51 +0200

    test

--- pages/16_test.py
+++ pages/16_test.py
@@ -3,9 +3,14 @@ from streamlit_plotly_events import plotly_events
 import numpy as np
 import plotly.express as px
 
-x = np.random.rand(100)
-y = np.random.rand(100)
+@st.cache_data
+def toto():
+    x = np.random.rand(100)
+    y = np.random.rand(100)
+    return x,y
 # Writes a component similar to st.write()
+
+x,y=toto()
 fig = px.scatter(x=x, y=y)
 selected_points = plotly_events(fig,click_event=True)
 st.write(selected_points)
