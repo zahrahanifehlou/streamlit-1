d2eca73b9675ac54f2a5a77ca6df6525376654f2
cyrenaique <arno.ogier@gmail.com>
2023-10-12 10:32:10 +0200

    cache_

--- pages/16_test.py
+++ pages/16_test.py
@@ -13,16 +13,31 @@ profile_conn = psycopg2.connect(
     password="12345",
 )
 
+
+conn = psycopg2.connect(
+    host="192.168.2.131",
+    port="5432",
+    user="arno",
+    database="ksi_cpds",
+    password="12345",
+)
+
 @st.cache_data
 def toto():
-    sql_umqpemd = f"select * from umapemd where metasource='CRISPER'"
+    sql_umqpemd = f"select * from umapemd where metasource='Ksilink_625'"
     df_src_emd = sql_df(sql_umqpemd, profile_conn)
     return df_src_emd
 # Writes a component similar to st.write()
 
 
 df=toto()
+# st.write(df)
 fig = px.scatter(df,x="umap1",
                 y="umap2",)
 selected_points = plotly_events(fig,click_event=True)
-st.write(df.iloc[selected_points[0]['pointIndex']])
+if selected_points:
+    batch = df.iloc[selected_points[0]['pointIndex']].metabatchid
+    sql_test = f"select * from platemap where batchid='{batch}' and source='Ksilink_625'"
+    df_test_emd = sql_df(sql_test, conn)
+    st.write(sql_test)
+    st.write(df_test_emd)
