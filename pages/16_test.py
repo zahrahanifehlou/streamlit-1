608fb4292ce6c473343775a69e6b31520442c869
cyrenaique <arno.ogier@gmail.com>
2023-10-12 23:47:02 +0200

    diag

--- pages/16_test.py
+++ pages/16_test.py
@@ -46,7 +46,7 @@ emd_pca = pd.DataFrame()
 emd_pca["pca1"] = projection_2d[:, :1].flatten()
 emd_pca["pca2"] = projection_2d[:, 1:2].flatten()
 kmeans = KMeans(n_clusters=nb_cluster, random_state=0).fit(emd_pca)
-emd_pca[meta_cols] = df[meta_cols]
+# emd_pca[meta_cols] = df[meta_cols]
 
 emd_pca['Cluster']=kmeans.labels_
 emd_pca['Cluster'] = emd_pca['Cluster'].astype(str)
@@ -58,23 +58,23 @@ fig1 = px.scatter(
             
                
                 title=f" PCA ",
-                hover_data=["metabatchid"],
+                # hover_data=["metabatchid"],
             )
             
 st.plotly_chart(fig1, theme="streamlit",
                                 use_container_width=True)
 
 ## UMAP
-from umap import UMAP
+import umap
 
-umap_2d = UMAP(n_components=2, n_neighbors=30,
+umap_2d = umap.UMAP(n_components=2, n_neighbors=30,
                 min_dist=0, verbose=True, random_state=42)
-
+mapper=umap_2d.fit(X)
 projection_2d = umap_2d.fit_transform(X)
 emd_umap = pd.DataFrame()
 emd_umap["umap1"] = projection_2d[:, :1].flatten()
 emd_umap["umap2"] = projection_2d[:, 1:2].flatten()
-emd_umap[meta_cols] = df[meta_cols]
+# emd_umap[meta_cols] = df[meta_cols]
 emd_umap['Cluster']=kmeans.labels_
 emd_umap['Cluster'] = emd_umap['Cluster'].astype(str)
 fig = px.scatter(
@@ -85,7 +85,7 @@ fig = px.scatter(
               
                 
                 title=f" UMAP ",
-                hover_data=["metabatchid"],
+                # hover_data=["metabatchid"],
             )
             
 st.plotly_chart(fig, theme="streamlit",
@@ -101,3 +101,61 @@ st.write(df_g1)
 st.write(df_g2)
 fig3=ff.create_quiver( df_g1["pca1"],df_g1["pca2"],(df_g2["umap1"]-df_g1["pca1"]),df_g2["umap2"]-df_g1["pca2"],scale=1)
 st.plotly_chart(fig3)
+import umap.plot
+fig6 = umap.plot.diagnostic(mapper, diagnostic_type='pca')
+# st.write(fig6)
+st.pyplot(fig6.axes.figure)
+
+fig7=umap.plot.diagnostic(mapper, diagnostic_type='vq')
+st.pyplot(fig7.axes.figure)
+
+fig8=umap.plot.diagnostic(mapper, diagnostic_type='local_dim')
+st.pyplot(fig8.axes.figure)
+
+fig9=umap.plot.diagnostic(mapper, diagnostic_type='neighborhood')
+st.pyplot(fig9.axes.figure)
+
+fig10 = umap.plot.connectivity(mapper, edge_bundling='hammer')
+st.pyplot(fig10.axes.figure)
+# X=df_g1.values
+
+# grid_unit_size = .02
+# margin = 0.25
+
+# x_min = X[:, 0].min() - margin
+# x_max = X[:, 0].max() + margin
+# y_min = X[:, 1].min() - margin
+# y_max = X[:, 1].max() + margin
+
+# x_range = np.arange(start = x_min, stop = x_max, step = grid_unit_size )
+# y_range = np.arange(start = y_min, stop = y_max, step = grid_unit_size )
+
+# x_gridvalues, y_gridvalues = np.meshgrid(x_range, y_range)
+
+# # COMPUTE PROBABILITIES ON GRID
+# gridvalues_combined_tidy = np.vstack([x_gridvalues.flatten(), y_gridvalues.flatten()]).T
+# # knn_class_probabilities = knn_classifier.predict_proba(gridvalues_combined_tidy)
+
+# probability_postive_class = knn_class_probabilities[:,1]
+
+# fig = go.Figure(data=[
+#     go.Contour(
+#         x = x_range
+#         ,y = y_range
+#         ,z = probability_postive_class.reshape(x_gridvalues.shape)
+#         ,colorscale = 'RdBu'
+#         #,alpha = .3
+#     )
+# ])
+ 
+# x = np.linspace(-4, 4, 9)
+# y = np.linspace(-5, 5, 11)
+# random_data = np.random.random((11, 9))
+# x_1, y_1 = np.meshgrid(df_g1["pca1"], df_g1["pca2"])
+# data1,data2 = np.meshgrid(df_g2["umap1"],df_g2["umap2"])
+
+# # data= np.concatenate(df_g2["umap1"].values,df_g2["umap2"].values)
+# st.write(data1)
+# fig6 = plt.plot_surface(x_1,y_1,data1, cmap = 'jet')
+
+# st.pyplot(fig6.axes.figure)
