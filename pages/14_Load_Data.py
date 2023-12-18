6ee0114600e51ec1fd5eed9a38585979ff33eddc
cyrenaique <arno.ogier@gmail.com>
2023-12-18 15:48:55 +0100

    cwt

--- pages/14_Load_Data.py
+++ pages/14_Load_Data.py
@@ -107,11 +107,40 @@ if len(list_df) > 0:
         )
         st.plotly_chart(fig3, theme="streamlit", use_container_width=True)
         # st.write(df_plt)
-        N=len(df_agg.columns)-1
+        N=len(time_cols)
+        T=1.0/30.0
+        import scipy
+        from scipy import signal
+        import matplotlib.pyplot as plt
+        sel_col_fft= st.selectbox("Chose exp",df_plt.columns)
+        yy=df_plt[sel_col_fft].values
+        yf = scipy.fftpack.fft(yy)
+        widths = np.arange(1, N//8)
+        xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+        cwtmatr = signal.cwt(yy, signal.ricker, widths)
+        cwtmatr_yflip = np.flipud(cwtmatr)
+        fig_cwt, ax = plt.subplots()
+        ax.imshow(cwtmatr_yflip,  cmap='PRGn', aspect='auto',
+           vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
+        st.pyplot(fig_cwt, use_container_width=True)
+        df_fft=pd.DataFrame()
+        df_fft['xf']=xf
+        df_fft['yf']=2.0/N * np.abs(yf[:N//2])
+        components.html(get_pyg_html(df_fft), height=1000, scrolling=True)
+        st.write('df_fft',df_fft)
+        # fig_fft = px.scatter(df_fft,xf,yf)
+        # st.plotly_chart(fig_fft, theme="streamlit", use_container_width=True)
+
+        # 
+
+        # ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+        
+        # # plt.show()
+        # st.pyplot(fig_fft, use_container_width=True)
         
     g = col2.radio("MinMax", ["yes", "no"])
     col_sel = data.select_dtypes(include=numerics).columns.to_list()
-    if g == "yes":
+    if g == "yes" and t=='no':
         scaler = MinMaxScaler()
         col_sel = data.select_dtypes(include=numerics).columns.to_list()
         cols_alpha = data.select_dtypes(exclude=numerics).columns
