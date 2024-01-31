fc971fa3cf72d64fef1fdfd8fedfb9253dd73bff
cyrenaique <arno.ogier@gmail.com>
2024-01-31 11:44:28 +0100

    pubchem3

--- pages/19_PubChem.py
+++ pages/19_PubChem.py
@@ -13,59 +13,14 @@ from query_pubchem import QueryPubChem
 
 pubchem = QueryPubChem()
 
-
-# def moa_annotation(field):
-#     moa = ["" for _ in range(len(field))]
-
-#     for x in range(len(field)):
-#         compound = field[x].split(",")
-#         # st.write(compound)
-#         for y in range(len(list(compound))):
-#             query_compound = list(compound)[y]
-#             # st.write(query_compound)
-#             response = requests.get(
-#                 "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{}/JSON?heading=FDA+Pharmacological+Classification".format(
-#                     query_compound
-#                 )
-#             )
-#             time.sleep(0.2)  # PubChem database allows only 5 queries per second
-#             # st.write(response)
-#             try:
-#                 moa_section = response.json()["Record"]["Section"][0]["Section"][0][
-#                     "Information"
-#                 ]
-#                 for i in range(len(moa_section)):
-#                     temp = moa_section[i]["Value"]["StringWithMarkup"][0][
-#                         "String"
-#                     ].split(" - ")
-#                     if temp[0] == "Mechanisms of Action [MoA]":
-#                         if moa[x] == "":
-#                             moa[x] = temp[-1]
-#                         else:
-#                             moa[x] = moa[x] + ", " + temp[-1]
-#             except KeyError:
-#                 continue
-
-#     return moa
-
+st.title("Pubchem Information")
+st.subheader("fetching compounds information from PubChem", divider="rainbow")
 
 compounds = pd.DataFrame()
 if "PubChem" not in st.session_state:
     exit(0)
 list_cpd = st.session_state["PubChem"]
-# list_cpd = [
-#     "2979901",
-#     "24772097",
-#     "11691242",
-#     "5154691",
-#     "1179237",
-#     "1104205",
-#     "16070100",
-#     "2915164",
-#     "41018128",
-#     "4996",
-# ]
-# compounds["pubchem_cid"] = list_cpd
+
 list_pub = []
 for item in list_cpd:
     list_pub.append(pcp.get_compounds(item, "cid", as_dataframe=True))
@@ -73,10 +28,13 @@ for item in list_cpd:
 compounds = pd.concat(list_pub).reset_index()
 compounds = compounds.astype({"cid": str})
 # st.write(compounds)
+st.warning("Getting MOA")
 compounds["pubchem_moa"] = pubchem.moa_annotation(compounds["cid"])
 
+st.warning("Getting bioassay")
 bioassay_df = pubchem.bioassay(compounds.cid)
 with_target = bioassay_df.dropna(subset=["Target GeneID"]).reset_index(drop=True)
+st.warning("Getting target, can be long...")
 with_target["GeneName"] = pubchem.target(with_target.AID)
 # st.write(with_target)
 median_activity_value = (
@@ -134,4 +92,4 @@ target_merge = target_merge.merge(
 #     ]
 # ]
 
-st.write(target_merge)
+st.write(f"Info from Pubchem for your {len(list_cpd)} compounds", target_merge)
