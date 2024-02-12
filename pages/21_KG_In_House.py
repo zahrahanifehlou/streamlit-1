f72c798c6318925d70f925744aec05f468c7da69
cyrenaique <arno.ogier@gmail.com>
2024-02-12 11:53:39 +0100

    KGH

--- pages/21_KG_In_House.py
+++ pages/21_KG_In_House.py
@@ -62,6 +62,58 @@ simi_df_alt = simi_df_alt.stack().reset_index()
 simi_df_alt.columns = ["source", "target", "similarity"]
 
 
-simi_high = simi_df_alt[simi_df_alt["similarity"] > 0.97]
+simi_high = simi_df_alt[simi_df_alt["similarity"] > 0.9]
 st.write(simi_high.reset_index(drop=True))
-draw_graph(simi_high, 0, "cpds")
+
+G = nx.from_pandas_edgelist(simi_high)
+st.write(G)
+# node_and_degree = G.degree()
+# most_connected_node = sorted(G.degree, key=lambda x: x[1], reverse=True)[0]
+# degree = G.degree(most_connected_node)
+
+# # Create egonet for the focal node
+# hub_ego = nx.ego_graph(G, most_connected_node[0])
+
+# Create the equivalent Node and Edge lists
+nodes = [Node(id=i, label=str(i), size=20) for i in (G.nodes)]
+edges = [
+    Edge(source=i, target=j, type="CURVE_SMOOTH")
+    for (i, j) in G.edges
+    # if i in hub_ego.nodes and j in hub_ego.nodes
+]
+
+
+config = Config(
+    width=1200,
+    height=1200,
+    directed=False,
+    nodeHighlightBehavior=True,
+    highlightColor="#F7A7A6",
+    collapsible=True,
+    node={"labelProperty": "label"},
+    link={"labelProperty": "label", "renderLabel": True},
+)
+
+return_value = agraph(nodes=nodes, edges=edges, config=config)
+# # st.write(GG.nodes())
+
+# # get_nodes = GG.nodes()
+
+# # keep = [node for node, degree in dict(GG.degree()).items() if degree > 0]
+
+# # remove = get_nodes - keep
+
+# # GG.remove_nodes_from(remove)
+
+# net = Network(notebook=False)
+# net = net.from_nx(GG)
+
+# html = "tests"
+# # net.show_buttons(filter_="physics")
+# net.show(f"/mnt/shares/L/Temp/{html}.html", notebook=False)
+
+# HtmlFile = open(f"/mnt/shares/L/Temp/{html}.html", "r", encoding="utf-8")
+# source_code = HtmlFile.read()
+# components.html(source_code, height=1200, width=1200)
+
+# # draw_graph(simi_high, 0, "cpds")
