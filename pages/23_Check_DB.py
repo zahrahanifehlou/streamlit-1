7b1481a3cda7e097010824dfa768f6781d235f18
cyrenaique <arno.ogier@gmail.com>
2024-03-05 09:43:20 +0100

    thr

--- pages/23_Check_DB.py
+++ pages/23_Check_DB.py
@@ -71,6 +71,43 @@ output = llm.create_chat_completion(
 )
 
 
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
+
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings
+from llama_index.core.query_engine import PandasQueryEngine
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
+from llama_index.core.chat_engine import SimpleChatEngine
+
+
+def llama():
+    llm = LlamaCPP(
+        # You can pass in the URL to a GGML model to download it automatically
+        # model_url=model_url,
+        # optionally, you can set the path to a pre-downloaded model instead of model_url
+        model_path="/mnt/shares/L/PROJECTS/JUMP-CRISPR/Code/streamlit_arno/mistral-ins-7b-q4/mistral-7b-instruct-v0.2.Q4_K_M.gguf",
+        temperature=0.1,
+        max_new_tokens=2560,
+        # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+        context_window=3900,
+        # kwargs to pass to __call__()
+        generate_kwargs={},
+        # kwargs to pass to __init__()
+        # set to at least 1 to use GPU
+        # model_kwargs={"n_gpu_layers": 1},
+        # transform inputs into Llama2 format
+        messages_to_prompt=messages_to_prompt,
+        completion_to_prompt=completion_to_prompt,
+        verbose=True,
+    )
+    return llm
+
+
 # # Define a function to handle the conversation flow
 # def handle_conversation(messages):
 #     responses = []
