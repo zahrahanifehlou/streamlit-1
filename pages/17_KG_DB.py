357b0cf2df293c352dc4291e9ba3aeb0ada79160
zahra <zahra.hanifehlou@ksilink.com>
2024-04-23 22:27:58 +0200

    graph

--- pages/17_KG_DB.py
+++ pages/17_KG_DB.py
@@ -29,32 +29,31 @@ var_text = st.text_area(
     "Enter your list of entities", help="Name or ID separated by enter"
 )
 var_t = var_text.split("\n")
-depth = st.slider("Select depth:", 0, 4, 1)
-deg = st.slider("Select Degree:", 0, 10, 2)
+col1, col2 = st.columns(2)
+with col1:
+    depth = st.slider("Select depth:", 0, 4, 1)
+with col2:
+    deg = st.slider("Select Degree:", 0, 10, 0)
 list_gene = [t.strip().upper() for t in var_t if t != ""]
 if len(list_gene) > 0:
     gsql = (
         f"""SELECT * from cypher('%s', $$ MATCH p=(n )<-[r*{depth}]-() 
         where n.__id__ in {list_gene} 
-        WITH  p,count(r) as rel_cnt
-        WHERE rel_cnt > {deg}
         RETURN  p   $$) as (v agtype)"""
         % graphName
     )
     st.write(gsql)
     G = networkx.age_to_networkx(conn, graphName, query=gsql)
+    remove = [x for x in G.nodes() if G.degree(x) <= deg]
+    G.remove_nodes_from(remove)
+    
     label_dict = dict(G.nodes(data="properties", default=1))
     converted_dict = {key: value["__id__"] for key, value in label_dict.items()}
     H = nx.relabel_nodes(G, converted_dict)
-    # H = nx.relabel_nodes(G, converted_dict)
-    # pos = nx.spring_layout(H)
-    # nx.draw_networkx_labels(H, pos)
-    # nx.draw_networkx_edges(H, pos, edge_color="r", arrows=False)
-    # nx.draw_networkx_nodes(H, pos)
 
     net = Network(notebook=False)
     net.from_nx(H)
-    # st.write(net.nodes)
+
     nodes = [
         Node(
             id=u["id"],
