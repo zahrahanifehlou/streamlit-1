2840433d64593668d189d49a441afb79a9b5bd59
zahra <zahra.hanifehlou@ksilink.com>
2024-04-23 18:16:06 +0200

    graph

--- pages/17_KG_DB.py
+++ pages/17_KG_DB.py
@@ -35,10 +35,11 @@ var_text = st.text_area(
     "Enter your list of entities", help="Name or ID separated by enter"
 )
 var_t = var_text.split("\n")
+deg = st.slider("Select Degree:", 0, 10, 2)
 list_gene = [t.strip().upper() for t in var_t if t != ""]
 if len(list_gene) >0:
     gsql = (
-        f"""SELECT * from cypher('%s', $$ MATCH p=(n )<-[r*2]-() 
+        f"""SELECT * from cypher('%s', $$ MATCH p=(n )<-[r*{deg}]-() 
         where n.__id__ in {list_gene} 
         RETURN  p   $$) as (v agtype)"""
         % graphName
@@ -55,6 +56,39 @@ if len(list_gene) >0:
     nx.draw_networkx_edges(H, pos, edge_color='r', arrows = True)
     nx.draw_networkx_nodes(H,pos)
  
-    # nt = Network(notebook=False)
-    # nt.from_nx(H)
-    # nt.show('nx.html')
+    net = Network(notebook=False)
+    net.from_nx(H)
+    #st.write(net.nodes)
+    nodes = [
+                Node(
+                    id=u["id"],
+                    label=u["id"],
+                    color=u["color"],
+                    # title=list_desc[i],
+                    shape=u["shape"],
+                    font="10px arial grey",
+                    size=15,
+                )
+                for u in net.nodes
+            ]
+
+    edges = [
+                Edge(
+                    source=v["from"],
+                    target=v["to"],
+                   # title=v["title"],
+                   # color=v["color"],
+                )
+                for v in net.edges
+            ]
+   
+   
+    c = ConfigBuilder(
+        nodes,
+        edges,
+    )
+    d = c.build(dictify=False)
+    return_value = agraph(nodes, edges, config=d)
+
+
+    
